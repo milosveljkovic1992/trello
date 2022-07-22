fb9d8ad4212793fa664085214135827888aef627
Milos Veljkovic Velja <milos.veljkovic@htecgroup.com>
2022-07-22 15:56:39 +0200

    fix moving to empty list

--- src/components/organisms/card-move/card-move.js
+++ src/components/organisms/card-move/card-move.js
@@ -1,93 +1,62 @@
 import { useState, useEffect } from 'react';
 
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-import axios from 'axios';
 import { GrClose } from 'react-icons/gr';
 
 import { Container } from './card-move-styles';
-import { throwError } from 'store/error-slice';
+import { nanoid } from '@reduxjs/toolkit';
 
 export const CardMove = ({ rect, card, setIsMoveOpen, handleMove, index }) => {
-  const dispatch = useDispatch();
+  const allLists = useSelector((state) => state.lists.listsArray);
+  const cards = useSelector((state) => state.cards.cardsArray);
+
   const [isLoading, setIsLoading] = useState(true);
-  const [allLists, setAllLists] = useState(null);
   const [currentList, setCurrentList] = useState(null);
   const [selectedList, setSelectedList] = useState(null);
   const [selectedListId, setSelectedListId] = useState(null);
   const [selectedPosition, setSelectedPosition] = useState(1);
 
-  const { boardId } = useParams();
-
   const getListsInfo = () => {
-    const fetchAllLists = async () => {
-      try {
-        const response = await axios.get(`/1/boards/${boardId}/lists`);
-        setAllLists(response.data);
-      } catch (error) {
-        dispatch(throwError('Ooops something went wrong'));
-      }
-    };
-
-    const fetchCurrentList = async () => {
-      try {
-        const response = await axios.get(`/1/lists/${card.idList}/cards`);
-        setCurrentList(response.data);
-        setSelectedList(response.data);
-        setSelectedListId(response.data[0].idList);
-      } catch (error) {
-        dispatch(throwError('Ooops something went wrong'));
-      }
-    };
-
-    fetchAllLists();
-    fetchCurrentList();
+    setCurrentList(cards.filter(({ idList }) => card.idList === idList));
+    setSelectedList(cards.filter(({ idList }) => card.idList === idList));
+    setSelectedListId(card.idList);
   };
 
   const handleSelect = (e) => {
     const listId = e.target.value;
 
-    const fetchSelectedList = async () => {
-      try {
-        const response = await axios.get(`/1/lists/${listId}/cards`);
-        setSelectedList(response.data);
-        setSelectedListId(response.data[0].idList);
-        setSelectedPosition(1);
-        if (response.data.length > 0) {
-          setSelectedPosition(response.data[0].pos - 1);
-        }
-      } catch (error) {
-        dispatch(throwError('Ooops something went wrong'));
-      }
-    };
-
-    fetchSelectedList();
-    console.log(selectedPosition);
+    setSelectedList(cards.filter((card) => card.idList === listId));
+    setSelectedListId(listId);
+    setSelectedPosition(1);
+    console.log('handle select');
   };
 
   const handlePosition = (e) => {
+    console.log('handle position');
     const targetPosition = Number(e.target.value);
 
     const isSameList = currentList[0].idList === selectedList[0].idList;
     const isLastIndex = targetPosition === selectedList.length - 1;
     const isLastItemOnAnotherList = targetPosition === selectedList.length;
 
-    if (targetPosition === 0) {
-      setSelectedPosition(Math.round(selectedList[0].pos / 2) - 1);
-    } else if (isSameList && isLastIndex) {
-      setSelectedPosition(selectedList[targetPosition].pos + 11000);
-    } else if (isLastItemOnAnotherList) {
-      setSelectedPosition(selectedList[index - 1].pos + 10000);
-    } else if (targetPosition > index) {
-      setSelectedPosition(selectedList[targetPosition].pos + 1);
-    } else {
-      setSelectedPosition(
-        selectedList[targetPosition].pos -
-          (selectedList[targetPosition].pos -
-            selectedList[targetPosition - 1].pos) /
-            2,
-      );
+    if (selectedList.length > 0) {
+      if (targetPosition === 0) {
+        setSelectedPosition(Math.round(selectedList[0].pos / 2) - 1);
+      } else if (isSameList && isLastIndex) {
+        setSelectedPosition(selectedList[targetPosition].pos + 11000);
+      } else if (isLastItemOnAnotherList) {
+        setSelectedPosition(selectedList[targetPosition - 1].pos + 10000);
+      } else if (isSameList && targetPosition > index) {
+        setSelectedPosition(selectedList[targetPosition].pos + 1);
+      } else {
+        setSelectedPosition(
+          selectedList[targetPosition].pos -
+            (selectedList[targetPosition].pos -
+              selectedList[targetPosition - 1].pos) /
+              2,
+        );
+      }
     }
   };
 
@@ -135,19 +104,24 @@ export const CardMove = ({ rect, card, setIsMoveOpen, handleMove, index }) => {
         <div className="dropdown-container position-dropdown">
           <div className="dropdown-label">Position</div>
           <select className="dropdown" onChange={handlePosition}>
-            {allLists &&
-              selectedList &&
+            {allLists && selectedList.length > 0 ? (
               selectedList.map((option, index) => (
                 <option key={`option-${option.id}`} value={index}>
                   {index + 1}
                 </option>
-              ))}
-
-            {currentList[0].idList !== selectedList[0].idList && (
-              <option value={selectedList.length}>
-                {selectedList.length + 1}
+              ))
+            ) : (
+              <option key={`option-${nanoid()}`} value={0}>
+                1
               </option>
             )}
+
+            {selectedList.length > 0 &&
+              currentList[0].idList !== selectedList[0].idList && (
+                <option value={selectedList.length}>
+                  {selectedList.length + 1}
+                </option>
+              )}
           </select>
         </div>
       </div>
