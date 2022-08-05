5af13561d4963f0ea6edf3599f5132edd7bf6e9a
Milos Veljkovic Velja <milos.veljkovic@htecgroup.com>
2022-08-05 16:16:20 +0200

    add single card unit tests

--- src/components/molecules/single-card/single-card.tsx
+++ src/components/molecules/single-card/single-card.tsx
@@ -22,7 +22,12 @@ export const SingleCard = ({
   const editPanelProps = useSingleCard({ setIsListUpdated, card });
 
   return (
-    <Container ref={editPanelProps.cardRef} draggable className="card-box">
+    <Container
+      ref={editPanelProps.cardRef}
+      draggable
+      className="card-box"
+      data-testid="single-card"
+    >
       <Link
         to={`c/${card.id}`}
         draggable
@@ -30,11 +35,12 @@ export const SingleCard = ({
         onDragStart={(e) => handleDragStart(e, card, index)}
         onDragEnter={(e) => handleDragEnterCard(e, card, index)}
         onDragEnd={(e) => handleDragEnd(e)}
+        data-testid="single-card-link"
       >
         <div className="card-content-box">
           <p className="card-title">{card.name}</p>
           {(card.badges.description || !!card.badges.comments) && (
-            <div className="badges">
+            <div className="badges" data-testid="card-badges">
               {card.badges.description && (
                 <div className="badge-icon-container">
                   <span className="badge-icon">
@@ -60,6 +66,7 @@ export const SingleCard = ({
       <div
         className="edit-btn"
         onClick={() => editPanelProps.setIsEditOpen(true)}
+        data-testid="edit-button"
       >
         <TbPencil />
       </div>
