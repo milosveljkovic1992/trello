870ac54f204b5bbcb818de845c63bf3838fc0f16
Milos Veljkovic Velja <milos.veljkovic@htecgroup.com>
2022-08-15 12:55:16 +0200

    merge conflict on develop branch

--- src/components/atoms/new-card/new-card.tsx
+++ src/components/atoms/new-card/new-card.tsx
@@ -4,10 +4,13 @@ import { NewCardProps } from './new-card.types';
 import { NewItem } from 'components/atoms';
 
 export const NewCard = ({ setIsCreatingNew, listId }: NewCardProps) => {
+  // TODO update hook logic
   const { handleInput, handleSubmit } = useNewCard({
     setIsCreatingNew,
     listId,
   });
+  // TODO: update logic
+  console.log(listId);
 
   return (
     <NewItem
