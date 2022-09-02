de066d42961b79efc4ef5432270b03c38089699f
Milos Veljkovic <milos.veljkovic@htecgroup.com>
2022-09-02 14:52:16 +0000

    style: calculate position (pull request #46)

--- src/utils/calculatePosition.ts
+++ src/utils/calculatePosition.ts
@@ -18,11 +18,27 @@ export const calculatePosition = ({
   let isSameList = false;
   const itemsInTargetList = targetList.length;
 
+  // if there are no cards in target list
+  // return pos default value which is 1000
   if (itemsInTargetList > 0) {
     isSameList = previousList[0].idList === targetList[0].idList;
 
+    // If target list is same as previous list
+    // last index will have value of list.length - 1
+
+    // Example: list contains 3 cards and we wish to transfer
+    // a card from first to last place. Cards would contain indexes 0, 1, 2
+    // so that would mean the target index would be 2 which is list.length - 1
     const isLastIndexOnSameList =
       isSameList && targetIndex === itemsInTargetList - 1;
+
+
+    // If target list is NOT the same as previous list
+    // last index will have value of list.length
+
+    // Example: target list contains 3 cards and we wish to transfer a card 
+    // to the last index. Other cards would keep their current indexes 0, 1, 2
+    // and the target index would be index 3 which is the same as list.length
     const isLastItemOnAnotherList =
       !isSameList && targetIndex === itemsInTargetList;
 
@@ -30,32 +46,47 @@ export const calculatePosition = ({
 
     if (targetIndex === 0) {
       const firstItemPosition = targetList[0].pos;
-      const firstItemHalfPositionValue = targetList[0].pos / 2;
-
+      const firstItemPositionQuotient = firstItemPosition / 1.1;
       let subtractor = 1;
-        while (firstItemHalfPositionValue - subtractor < subtractor * 10) {
-          subtractor = subtractor / 10;
-        }
+
+      // this ensures that result never becomes 
+      // negative when subtracting from quotient
+      while (firstItemPositionQuotient - subtractor < subtractor * 10) {
+        subtractor = subtractor / 10;
+      }
       
-      pos = firstItemHalfPositionValue > 2 ? firstItemHalfPositionValue : Number((firstItemPosition - subtractor).toFixed(5));
-      
+      pos = Number((firstItemPositionQuotient - subtractor).toFixed(5));
+
     } else if (isLastIndexOnSameList) {
+      // this adds 10000 (arbitrary value) to last index's 
+      // position to ensure our card would become last
       pos = itemCurrentlyOnTargetIndex.pos + 10000;
 
     } else if (isLastItemOnAnotherList) {
+      // this adds 10000 (arbitrary value) to last index's 
+      // position to ensure our card would become last
       const lastItemOnAnotherList = targetList[targetIndex - 1];
       pos = lastItemOnAnotherList.pos + 10000;
 
     } else if (isSameList && targetIndex > previousIndex) {
+      // this means we are moving the card down the list
+
+      // We take the [targetindex].pos and [item after targetindex].pos value and split the difference.
+      // We are confident an item after target will actually exist because we assured in previous
+      // cases to check if [targetindex] is last index
       const itemAfterTargetIndex = targetList[targetIndex + 1];
       const targetAndNextItemPositionDifference = itemAfterTargetIndex.pos - itemCurrentlyOnTargetIndex.pos;
       pos = itemCurrentlyOnTargetIndex.pos + (targetAndNextItemPositionDifference / 2);
 
     } else {
+       // this means we are moving the card up the list
+
+      // We take the [targetindex].pos and [item before targetindex].pos value and split the difference.
+      // We are confident an item before target will actually exist because we assured in previous
+      // cases to check if [targetindex] is index 0
       const itemBeforeTargetIndex = targetList[targetIndex - 1];
       const targetAndPreviousItemPositionDifference = itemCurrentlyOnTargetIndex.pos - itemBeforeTargetIndex.pos;
       pos = itemCurrentlyOnTargetIndex.pos - (targetAndPreviousItemPositionDifference / 2);
-
     }
   }
   return pos;
